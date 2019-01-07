4ef62ce14c51efdb26106877de0e8505eb33fdb1
nbeebz <bhatti.nav@gmail.com>
2019-01-07 15:52:12 -0800

    Added simple command line parsing and options for Mandlebox params and image size. Also generate directories for image output

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -10,15 +10,19 @@
 #include <ctime>
 #include <iomanip> // put_time
 #include <time.h>
+#include <Windows.h>
 
 #include "Utility/PNG/png.h"
 #include "Utility/PNG/pngUtil.h"
 #include "Utility/Image/Image.h"
+#include "Utility/CLI/CommandLineParser.h"
 
 #include "Fractal/MandelBox/MandelBox.h"
 
 
 #define _IMAGE_SIZE_ 256 
+
+void DrawBox(MandelBox mandelBox, int width, int height, std::string dir);
 
 std::string CurrentTimeAndDate()
 {
@@ -41,42 +45,68 @@ std::string CurrentTimeAndDate()
 	return ret.substr( 0, ret.size() - 1 );
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	const float rad = 6.f;
+	std::string directory = "D:\\Projects\\ContinuousFractals\\out";
+	CLI::Match("-dir", argc, argv, [&directory](char* param) {directory = param; });
+	directory += "\\" + CurrentTimeAndDate();
+
+	uint32_t width = _IMAGE_SIZE_, height = _IMAGE_SIZE_;
+	CLI::Match("-img", argc, argv, [&width, &height](char* param) {
+		sscanf_s(param, "%dx%d", &width, &height);
+	});
+
+	MandelBox mandelBox(2.f, 150);
+	bool drawBox = CLI::Match("-box", argc, argv, [&mandelBox](char* param){
+		float formulaScale = 2.f;
+		int iterationCount = 150;
+		sscanf_s(param, "%f,%d", &formulaScale, &iterationCount);
+
+		mandelBox = MandelBox(formulaScale, iterationCount);
+	});
+
+	
+	if (!CreateDirectoryA(directory.c_str(), NULL))
+	{
+		std::cout << "Error creating directory to save fractal output";
+	}
+
+	DrawBox(mandelBox, height, width, directory);
+	
+    return 0;
+}
+
+void DrawBox(MandelBox mandelBox, int width, int height, std::string dir)
+{
+	const std::string filename("mandelBox");
+	const std::string extension(".png");
+
 	const float increment = -0.0001f;
-	float current = rad;
+	const float rad = 6.f;
 	int count = 10;
-	while( current > -rad && count > 0)
+	float current = rad;
+	while (current > -rad && count > 0)
 	{
 		count--;
-		MandelBox mandelBox( 2.f, 150 );
-
-		const uint32_t width = _IMAGE_SIZE_, height = _IMAGE_SIZE_;
-
-		const std::string directory( "D:\\Projects\\ContinuousFractals\\out\\" );
-		const std::string filename( "mandelBox" );
-		const std::string extension( ".png" );
 
 		std::stringstream dimensionsStream;
 		dimensionsStream << width << "_x_" << height << "_" << mandelBox.GetParamDesc() << "_" << "z=" << current;
-		const std::string dimensions( dimensionsStream.str() );
+		const std::string dimensions(dimensionsStream.str());
 
 
-		const std::string fullpath = directory + filename + dimensions + CurrentTimeAndDate() + extension;
-		Image::Image image( width, height, fullpath );
+		const std::string fullpath = dir + "\\" + filename + dimensions + extension;
+		Image::Image image(width, height, fullpath);
 
 		FractalGenerator3D mandelBoxGenerator;
 		FractalGenerator3D::GenerateParams params;
-		params.Origin = Vector3f( 0.f, 0.f, current );
-		params.scale = Vector3f( 15.0f );
-		mandelBoxGenerator.Generate( image, params, mandelBox );
+		params.Origin = Vector3f(0.f, 0.f, current);
+		params.scale = Vector3f(15.0f);
+		mandelBoxGenerator.Generate(image, params, mandelBox);
 
 		// write to file
 		image.Save();
 
 		current += increment;
 	}
-    return 0;
 }
 
---
+++ src/Utility/CLI/CommandLineParser.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <functional>
+#include <sstream>
+
+namespace CLI
+{
+	// Utility function for quick and dirty command line parsing
+	// TODO: Real parameter handling
+	static bool Match(const char* op, int argc, char* argv[], std::function<void(char*)> action)
+	{
+		for (int i = 0; i < argc; ++i)
+		{
+			if (strcmp(op, argv[i]) == 0)
+			{
+				if (i + 1 <= argc)
+				{
+					action(argv[i + 1]);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	static char** SplitParams(char* params)
+	{
+
+	}
+
+}

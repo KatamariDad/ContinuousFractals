f2349f92c5bcbf925f0b9ad38804f92203ce0b8e
nbeebz <bhatti.nav@gmail.com>
2019-01-27 23:44:43 -0800

    Make gifs a little more robust

--- Core/src/Image/GIF/GifUtil.h
+++ Core/src/Image/GIF/GifUtil.h
@@ -13,20 +13,23 @@ namespace Image
 			, m_width(width)
 			, m_height(height)
 		{
-			m_delay = 100.f / fps;
+			m_delay = uint32_t(100.f / fps);
 		};
 
-		// Don't add an image with a size less than the gif's!
 		void AddFrame( const Image& image )
 		{
-			int size = m_width * m_height * 4;
+			uint32_t size = m_width * m_height * 4;
 			std::vector<uint8_t> frame( size );
-			int i = 0;
-			for (int j = 0; j < size; )
+			uint32_t i = 0, j = 0;
+			while(i < image.m_width * image.m_height * 3 )
 			{
 				frame[j++] = image.m_pixels[i++];
 				frame[j++] = image.m_pixels[i++];
 				frame[j++] = image.m_pixels[i++];
+				frame[j++] = 0;
+			}
+			while (j < size)
+			{
 				frame[j++] = 0;
 			}
 			m_frames.push_back( frame );
@@ -34,13 +37,13 @@ namespace Image
 
 		void Save()
 		{
-			GifWriter* writer = nullptr; 
-			GifBegin( writer, m_filename, m_width, m_height, m_delay);
+			GifWriter writer; 
+			GifBegin( &writer, m_filename, m_width, m_height, m_delay);
 			for (auto frame : m_frames)
 			{
-				GifWriteFrame( writer, frame.data(), m_width, m_height, m_delay );
+				GifWriteFrame( &writer, frame.data(), m_width, m_height, m_delay );
 			}
-			GifEnd( writer );
+			GifEnd( &writer );
 		}
 	private:
 		std::vector<std::vector<uint8_t>> m_frames;
